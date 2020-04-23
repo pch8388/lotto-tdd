8a315872b4a2672784a70a3f6a1eac65eb37177d
pch8388 <pch838811@gmail.com>
2020-04-24 00:47:33 +0900

    [feat] : 로또티켓에 같은 번호가 중복으로 들어가면 안된다 - green

--- src/main/java/tdd/study/domain/LottoTicket.java
+++ src/main/java/tdd/study/domain/LottoTicket.java
@@ -1,20 +1,24 @@
 package tdd.study.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoTicket {
 
     private static final int LOTTO_SIZE = 6;
-    private final List<Integer> lottoNumbers;
+    private final Set<Integer> lottoNumbers;
 
-    private LottoTicket(List<Integer> numbers) {
+    private LottoTicket(Set<Integer> numbers) {
         this.lottoNumbers = numbers;
     }
 
     public static LottoTicket of(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
+        Set<Integer> lottoNumbers = new HashSet<>(numbers);
+        if (lottoNumbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("로또 사이즈는 6개!!");
         }
-        return new LottoTicket(numbers);
+
+        return new LottoTicket(lottoNumbers);
     }
 }
