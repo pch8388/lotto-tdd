363eac316e8e2d38d2d274d3f7b818f9822425d7
pch8388 <pch838811@gmail.com>
2020-04-24 00:06:48 +0900

    [feat] : 번호가 같으면 같은 객체로 간주한다 - test green

--- src/main/java/tdd/study/domain/LottoNumber.java
+++ src/main/java/tdd/study/domain/LottoNumber.java
@@ -1,10 +1,25 @@
 package tdd.study.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
 
     private int number;
 
     public LottoNumber(int number) {
         this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber number1 = (LottoNumber) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
