148d2347ff13a6b82cd450f64a5f18375a4bfc7e
pch8388 <pch838811@gmail.com>
2020-04-24 00:42:54 +0900

    [feat] : 로또 티켓 한장은 6자리의 숫자를 가져야한다 - green

--- src/main/java/tdd/study/domain/LottoTicket.java
+++ src/main/java/tdd/study/domain/LottoTicket.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class LottoTicket {
 
+    private final List<Integer> lottoNumbers;
+
+    private LottoTicket(List<Integer> numbers) {
+        this.lottoNumbers = numbers;
+    }
+
     public static LottoTicket of(List<Integer> numbers) {
-        return null;
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 사이즈는 6개!!");
+        }
+        return new LottoTicket(numbers);
     }
 }
