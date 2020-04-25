51d75004a7884fe73df6d49a695eab214f63583a
pch8388 <pch838811@gmail.com>
2020-04-25 23:29:07 +0900

    [refactor] : 매직넘버제거

--- src/main/java/tdd/study/domain/LottoStore.java
+++ src/main/java/tdd/study/domain/LottoStore.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 public class LottoStore {
 
+    private static final int UNIT_PRICE = 1000;
+
     public static List<LottoTicket> buy(int price) {
         List<LottoTicket> tickets = new ArrayList<>();
-        final int buyCount = price / 1000;
+        final int buyCount = price / UNIT_PRICE;
+
         for (int i = 0; i < buyCount; i++) {
             tickets.add(LottoCreator.create());
         }
