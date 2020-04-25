544134fc954d714f4534fed1d4db43716f2e22e0
pch8388 <pch838811@gmail.com>
2020-04-25 23:00:31 +0900

    [feat] : 티켓생성 - green

--- src/main/java/tdd/study/domain/LottoCreator.java
+++ src/main/java/tdd/study/domain/LottoCreator.java
@@ -1,8 +1,23 @@
 package tdd.study.domain;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
 public class LottoCreator {
 
+    private static final List<Integer> numbers;
+
+    static {
+        numbers = IntStream.rangeClosed(1, 45)
+            .boxed()
+            .collect(toList());
+    }
+
     public static LottoTicket create() {
-        return null;
+        Collections.shuffle(numbers);
+        return LottoTicket.of(numbers.subList(0, 6));
     }
 }
