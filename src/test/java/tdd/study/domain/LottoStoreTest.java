ed08be98dec1fd060ca62cb88c30b6755d9165f9
pch8388 <pch838811@gmail.com>
2020-04-25 23:32:47 +0900

    [test] : 정상적인 금액이 아니면 예외 발생 - red

--- src/test/java/tdd/study/domain/LottoStoreTest.java
+++ src/test/java/tdd/study/domain/LottoStoreTest.java
@@ -2,10 +2,13 @@ package tdd.study.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoStoreTest {
 
@@ -16,5 +19,13 @@ class LottoStoreTest {
         List<LottoTicket> tickets = LottoStore.buy(price);
 
         assertThat(tickets.size()).isEqualTo(10);
+    }
+
+    @DisplayName("정상적인 금액이 아니면 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {10500, -1000, 0, 12003})
+    public void validatePrice(int price) {
+        assertThatThrownBy(() -> LottoStore.buy(price))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
