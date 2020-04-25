d44417a5996ed08ee8bcc91ae8d8df60dd838ab4
pch8388 <pch838811@gmail.com>
2020-04-25 23:35:46 +0900

    [feat] : 정상적인 금액이 아니면 예외 발생 - green

--- src/main/java/tdd/study/domain/LottoStore.java
+++ src/main/java/tdd/study/domain/LottoStore.java
@@ -6,14 +6,25 @@ import java.util.List;
 public class LottoStore {
 
     private static final int UNIT_PRICE = 1000;
+    private static final String validMessage = "정상적인 금액이 아닙니다.";
 
     public static List<LottoTicket> buy(int price) {
-        List<LottoTicket> tickets = new ArrayList<>();
+        validatePrice(price);
         final int buyCount = price / UNIT_PRICE;
 
+        List<LottoTicket> tickets = new ArrayList<>();
         for (int i = 0; i < buyCount; i++) {
             tickets.add(LottoCreator.create());
         }
         return tickets;
+    }
+
+    private static void validatePrice(int price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException(validMessage);
+        }
+        if (price % UNIT_PRICE != 0) {
+            throw new IllegalArgumentException(validMessage);
+        }
     }
 }
