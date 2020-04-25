047799273a6e9ddaac18c7e0b1213d0f02166ba9
pch8388 <pch838811@gmail.com>
2020-04-26 02:50:25 +0900

    [feat] : 당첨된 로또의 랭크확인 - green

--- src/main/java/tdd/study/domain/Winner.java
+++ src/main/java/tdd/study/domain/Winner.java
@@ -1,14 +1,32 @@
 package tdd.study.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Winner {
 
-    public static Winner of(List<LottoTicket> tickets, LottoNumbers of) {
-        return null;
+    private static final long DEFAULT_VALUE = 0L;
+    private final Map<Rank, Long> winners;
+
+    private Winner(List<Rank> ranks) {
+        this.winners = ranks.stream()
+            .collect(groupingBy(identity(), counting()));
+    }
+
+    public static Winner of(List<LottoTicket> tickets, LottoNumbers winLotto) {
+        List<Rank> ranks = new ArrayList<>();
+        for (LottoTicket ticket : tickets) {
+            ranks.add(ticket.getRank(winLotto));
+        }
+        return new Winner(ranks);
     }
 
     public Long countOfWinner(Rank rank) {
-        return null;
+        return winners.getOrDefault(rank, DEFAULT_VALUE);
     }
 }
--- src/test/java/tdd/study/domain/WinnerTest.java
+++ src/test/java/tdd/study/domain/WinnerTest.java
@@ -15,9 +15,9 @@ class WinnerTest {
     public void rank() {
         List<LottoTicket> tickets = Arrays.asList(
             LottoTicket.of(LottoNumbers.of(Arrays.asList(1, 2, 3, 4, 5, 6))),
-            LottoTicket.of(LottoNumbers.of(Arrays.asList(3, 4, 6, 8, 9, 10))),
+            LottoTicket.of(LottoNumbers.of(Arrays.asList(3, 4, 5, 6, 9, 10))),
             LottoTicket.of(LottoNumbers.of(Arrays.asList(1, 2, 3, 6, 8, 9))),
-            LottoTicket.of(LottoNumbers.of(Arrays.asList(4, 5, 7, 8, 9, 12))),
+            LottoTicket.of(LottoNumbers.of(Arrays.asList(4, 5, 6, 8, 9, 12))),
             LottoTicket.of(LottoNumbers.of(Arrays.asList(1, 3, 16, 17, 39, 40))),
             LottoTicket.of(LottoNumbers.of(Arrays.asList(9, 12, 13, 34, 44, 45)))
         );
@@ -26,5 +26,6 @@ class WinnerTest {
         Winner winner = Winner.of(tickets, LottoNumbers.of(winNumbers));
         assertThat(winner.countOfWinner(Rank.FIRST)).isEqualTo(1);
         assertThat(winner.countOfWinner(Rank.FOURTH)).isEqualTo(2);
+        assertThat(winner.countOfWinner(Rank.FIFTH)).isEqualTo(1);
     }
 }
