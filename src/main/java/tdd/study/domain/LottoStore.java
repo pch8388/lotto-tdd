0e9948382ec0912b04f8950d2694a2efa28532b0
pch8388 <pch838811@gmail.com>
2020-04-25 23:38:27 +0900

    [refactor] : 매직넘버 제거

--- src/main/java/tdd/study/domain/LottoStore.java
+++ src/main/java/tdd/study/domain/LottoStore.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class LottoStore {
 
     private static final int UNIT_PRICE = 1000;
+    private static final int START_INDEX = 0;
+    private static final int VALID_NUMBER = 0;
     private static final String validMessage = "정상적인 금액이 아닙니다.";
 
     public static List<LottoTicket> buy(int price) {
@@ -13,17 +15,17 @@ public class LottoStore {
         final int buyCount = price / UNIT_PRICE;
 
         List<LottoTicket> tickets = new ArrayList<>();
-        for (int i = 0; i < buyCount; i++) {
+        for (int i = START_INDEX; i < buyCount; i++) {
             tickets.add(LottoCreator.create());
         }
         return tickets;
     }
 
     private static void validatePrice(int price) {
-        if (price <= 0) {
+        if (price <= VALID_NUMBER) {
             throw new IllegalArgumentException(validMessage);
         }
-        if (price % UNIT_PRICE != 0) {
+        if (price % UNIT_PRICE != VALID_NUMBER) {
             throw new IllegalArgumentException(validMessage);
         }
     }
