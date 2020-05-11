efd0703892e33a185f835d3754b63ffb6cdd9654
pch8388 <pch838811@gmail.com>
2020-05-11 23:33:10 +0900

    [refactor] : test 오류 수정

--- src/test/java/tdd/study/domain/LottoCreatorTest.java
+++ src/test/java/tdd/study/domain/LottoCreatorTest.java
@@ -3,6 +3,8 @@ package tdd.study.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoCreatorTest {
@@ -11,8 +13,10 @@ class LottoCreatorTest {
     @Test
     public void create() {
         LottoTicket lottoTicket = new LottoCreator().autoCreate();
+        final Rank rank =
+            lottoTicket.getRank(LottoNumbers.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
 
-        assertThat(lottoTicket.printLottoNumbers()).contains(",,,,,");
+        assertThat(rank).isInstanceOf(Rank.class);
     }
 
 }
